e009fba3e337aee453d67b0a19c6fb27a92bc5c9
Dominik Kapusta <d@ayoy.net>
2011-10-15 23:19:57 +0200

    Moving CGColorSpaceRef and CGColorRef creation outside -drawRect:
    * Color space is created in -initWithFrame: and released in -dealloc
    * CGColor is recreated with every glowColor change
    * Removing these operations from -drawRect: significantly improves
      drawing performance

--- Classes/RRSGlowLabel.m
+++ Classes/RRSGlowLabel.m
@@ -13,9 +13,21 @@
 
 @synthesize glowColor, glowOffset, glowAmount;
 
+- (void)setGlowColor:(UIColor *)newGlowColor
+{
+    if (newGlowColor != glowColor) {
+        [glowColor release];
+        CGColorRelease(glowColorRef);
+
+        glowColor = [newGlowColor retain];
+        glowColorRef = CGColorCreate(colorSpaceRef, CGColorGetComponents(glowColor.CGColor));
+    }
+}
+
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if(self != nil) {
+        colorSpaceRef = CGColorSpaceCreateDeviceRGB();
         self.glowOffset = CGSizeMake(0.0, 0.0);
         self.glowAmount = 0.0;
         self.glowColor = [UIColor clearColor];
@@ -28,19 +40,16 @@
     CGContextSaveGState(context);
     
     CGContextSetShadow(context, self.glowOffset, self.glowAmount);
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    
-    CGColorRef color = CGColorCreate(colorSpace, CGColorGetComponents(self.glowColor.CGColor));
-    CGContextSetShadowWithColor(context, self.glowOffset, self.glowAmount, color);
+    CGContextSetShadowWithColor(context, self.glowOffset, self.glowAmount, glowColorRef);
     
     [super drawTextInRect:rect];
     
-    CGColorRelease(color);
-    CGColorSpaceRelease(colorSpace);
     CGContextRestoreGState(context);
 }
 
 - (void)dealloc {
+    CGColorRelease(glowColorRef);
+    CGColorSpaceRelease(colorSpaceRef);
     [glowColor release];
     [super dealloc];
 }
